6b9c561bf043306a11ce309cedfebbfa3df65da4
nashwaharvani <124320552+nashwaharvani@users.noreply.github.com>
2026-05-20 13:40:12 +0700

    capstone7

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -5,20 +5,31 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 
+use App\Models\UserModel; 
+
 class AuthController extends BaseController
 {
-    
+    protected $userModel;
+    function __construct(){
+        $this->userModel = new UserModel();
+    }
 
 public function login()
 {
     if ($this->request->getPost()) {
+
+    $rules = [
+    'username' => 'required|min_length[6]',
+    'password' => 'required|min_length[7]|numeric',
+    ];
+    if ($this->validate($rules)) {
         $username = $this->request->getVar('username');
         $password = $this->request->getVar('password');
 
-        $dataUser = ['username' => 'april', 'password' => '202cb962ac59075b964b07152d234b70', 'role' => 'admin', 'email' => 'april@gmail.com']; // passw 123
+        $dataUser = $this->userModel ->where(['username' => $username])->first();
 
-        if ($username == $dataUser['username']) {
-            if (md5($password) == $dataUser['password']) {
+        if ($dataUser) {
+	    if (password_verify($password, $dataUser['password'])) {
                 session()->set([
                     'username' => $dataUser['username'],
                     'role' => $dataUser['role'],
@@ -35,6 +46,10 @@ public function login()
         } else {
             session()->setFlashdata('failed', 'Username Tidak Ditemukan');
             return redirect()->back();
+        }
+        } else {
+        session()->setFlashdata('failed', $this->validator->listErrors());
+        return redirect()->back();
         }
     } else {
         return view('v_login');
